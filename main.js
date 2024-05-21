485483063af426f73814123f16754b821d838a71
akhilkuamr <akhilkumar998@gmail.com>
2024-05-20 23:16:53 -0400

    5202024

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use("/images", express.static("uploads"));
 const corsOptions = {
-  origin: "http://e-arena.mind-pros.com",
+  origin: "https://e-arena.mind-pros.com",
   credentials: true,
   optionSuccessStatus: 200,
 };
